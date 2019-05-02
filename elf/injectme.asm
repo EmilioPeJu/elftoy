78ce9a89e43b4442938a087c4b849621b453afa7
hir12111 <hir12111@gmail.com>
2019-05-02 00:22:52 +0000

    Add example binary and example payload

---
+++ elf/injectme.asm
@@ -0,0 +1,30 @@
+[BITS 64]
+global _start
+section .text
+_start:
+; endbr64
+; db 0xf3, 0x0f, 0x1e, 0xfa
+push rax
+push rdi
+push rsi
+push rdx
+push rcx
+push r11
+mov rax, 1 ; write
+mov rdi, 0 ; stdout
+lea rsi, [rel msg] ; *buf
+mov rdx, 10    ; len
+syscall
+;mov rax, 60
+;mov rdi, 0
+;syscall
+pop r11
+pop rcx
+pop rdx
+pop rsi
+pop rdi
+pop rax
+jmp DWORD end
+end
+msg:
+db 'Inject me',0xa
